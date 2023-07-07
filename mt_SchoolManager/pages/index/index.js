ad4d862a127a9f64b4ac5b0d6d11901547bfb899
Tong‘s Macbook <tongmo@TongsdeMacBook-Pro.local>
2023-07-07 10:25:28 +0800

    增加了退出登录功能

--- mt_SchoolManager/pages/index/index.js
+++ mt_SchoolManager/pages/index/index.js
@@ -278,6 +278,17 @@ save(){
       })
      // console.log(that.data.uname)
   },
+  quit(){
+    app.globalData.stuId='';
+    app.globalData.password='';
+    wx.clearStorageSync()
+    wx.redirectTo({
+      url: '../login/login',
+      success: (res) => {},
+      fail: (res) => {},
+      complete: (res) => {},
+    })
+  },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
